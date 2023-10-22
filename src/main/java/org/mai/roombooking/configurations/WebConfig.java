56a8ca602c46c963b11a36bd4e2468c57fcc3b14
Grubenko Maxim <mdgrubenko@maieducation>
2023-10-22 23:28:00 +0300

    АРТЕМ НАХУЙ НЕ ТРОГАЙ МОИ ПАПОЧКИ

--- src/main/java/org/mai/roombooking/configurations/WebConfig.java
+++ src/main/java/org/mai/roombooking/configurations/WebConfig.java
@@ -1,4 +1,4 @@
-package org.mai.roombooking.utilities;
+package org.mai.roombooking.configurations;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
