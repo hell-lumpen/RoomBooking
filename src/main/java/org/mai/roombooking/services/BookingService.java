9f5833718deca1cef3bb011cd1b45ca42be1504c
Grubenko Maxim <mdgrubenko@maieducation>
2023-10-22 14:19:39 +0300

    add javadoc

--- src/main/java/org/mai/roombooking/services/BookingService.java
+++ src/main/java/org/mai/roombooking/services/BookingService.java
@@ -1,12 +1,5 @@
 package org.mai.roombooking.services;
 
-import org.mai.roombooking.entities.Booking;
-import org.mai.roombooking.entities.RecurrentUnit;
-import org.mai.roombooking.entities.dto.BookingDTO;
-import org.mai.roombooking.entities.dto.BookingDetailsDTO;
-import org.mai.roombooking.exceptions.BookingNotFoundException;
-import org.mai.roombooking.repositories.BookingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -14,37 +7,105 @@ import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+/**
+ * Сервисный класс для управления бронированиями.
+ */
 @Service
 public class BookingService {
 
+    /**
+     * Получает все бронирования комнат в заданном временном диапазоне.
+     *
+     * @param startTime дата и время начала запроса
+     * @param endTime   дата и время окончания запроса
+     * @return список бронирований комнат в заданном временном диапазоне
+     * @throws BookingNotFoundException если не найдены бронирования в указанный период
+     */
     public List<RoomBookingDTO> getBookingsInTimeRange(LocalDateTime startTime, LocalDateTime endTime) {
         return null;
     }
 
+    /**
+     * Получает бронирования для конкретной комнаты в заданном временном диапазоне.
+     *
+     * @param roomId    идентификатор комнаты
+     * @param startTime дата и время начала запроса
+     * @param endTime   дата и время окончания запроса
+     * @return список бронирований для указанной комнаты в заданном временном диапазоне
+     * @throws RoomNotFoundException    если комната не найдена по идентификатору
+     * @throws BookingNotFoundException если не найдены бронирования в указанный период
+     */
     public List<BookingDTO> getBookingsByRoomInTimeRange(Long roomId, LocalDateTime startTime, LocalDateTime endTime) {
         return null;
     }
 
+    /**
+     * Получает бронирования для конкретного пользователя в заданном временном диапазоне.
+     *
+     * @param userId    идентификатор пользователя
+     * @param startTime дата и время начала запроса
+     * @param endTime   дата и время окончания запроса
+     * @return список бронирований для указанного пользователя в заданном временном диапазоне
+     * @throws UserNotFoundException    если пользователь не найден по идентификатору
+     * @throws BookingNotFoundException если не найдены бронирования в указанный период
+     */
     public List<BookingDTO> getBookingsByUserInTimeRange(Long userId, LocalDateTime startTime, LocalDateTime endTime) {
         return null;
     }
 
+    /**
+     * Обновляет периодическое бронирование на основе предоставленного запроса.
+     *
+     * @param bookingId идентификатор бронирования для обновления
+     * @param request   запрос с информацией для обновления бронирования
+     * @return обновленное бронирование
+     * @throws BookingNotFoundException если бронирование не найдено по идентификатору
+     */
     public BookingDTO updatePeriodicBooking(Long bookingId, BookingUpdateRequestDTO request) {
         return null;
     }
 
+    /**
+     * Обновляет отдельное бронирование на основе предоставленного запроса.
+     *
+     * @param bookingId идентификатор бронирования для обновления
+     * @param request   запрос с информацией для обновления бронирования
+     * @return обновленное бронирование
+     * @throws BookingNotFoundException если бронирование не найдено по идентификатору
+     */
     public BookingDTO updateBooking(Long bookingId, BookingUpdateRequestDTO request) {
         return null;
     }
 
+    /**
+     * Удаляет периодическое бронирование на основе предоставленного идентификатора.
+     *
+     * @param bookingId идентификатор бронирования для удаления
+     * @throws BookingNotFoundException если бронирование не найдено по идентификатору
+     */
     public void deletePeriodicBooking(Long bookingId) {
-
+        // Реализация деталей
     }
 
+    /**
+     * Удаляет отдельное бронирование на основе предоставленного идентификатора.
+     *
+     * @param bookingId идентификатор бронирования для удаления
+     * @throws BookingNotFoundException если бронирование не найдено по идентификатору
+     */
     public void deleteBooking(Long bookingId) {
-
+        // Реализация деталей
     }
 
+    /**
+     * Создает новое бронирование на основе предоставленного запроса.
+     *
+     * @param request запрос с информацией для создания бронирования
+     * @return созданное бронирование
+     * @throws RoomNotFoundException   если комната не найдена по идентификатору
+     * @throws UserNotFoundException   если пользователь не найден по идентификатору
+     * @throws BookingConflictException если есть конфликт с существующим бронированием
+     */
     public BookingDTO createBooking(BookingCreateRequestDTO request) {
         return null;
     }
--- src/main/java/org/mai/roombooking/services/RoomService.java
+++ src/main/java/org/mai/roombooking/services/RoomService.java
@@ -4,14 +4,35 @@ package org.mai.roombooking.services;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
+/**
+ * Сервисный класс для управления комнатами.
+ */
 @Service
 public class RoomService {
+
+    /**
+     * Получает список доступных комнат в заданном временном диапазоне с использованием опциональных параметров.
+     *
+     * @param startTime     дата и время начала запроса на доступность
+     * @param endTime       дата и время окончания запроса на доступность
+     * @param capacity      опциональный параметр вместимости для фильтрации комнат
+     * @param hasProjector  опциональный параметр проектора для фильтрации комнат
+     * @param hasComputers  опциональный параметр компьютеров для фильтрации комнат
+     * @return список доступных комнат, соответствующих указанным критериям
+     */
     public List<RoomDTO> getAvailableRooms(LocalDateTime startTime, LocalDateTime endTime, Integer capacity, Boolean hasProjector, Boolean hasComputers) {
         return null;
     }
 
+    /**
+     * Получает список всех комнат со статусами, отсортированных по релевантности на основе опциональных параметров.
+     *
+     * @param capacity      опциональный параметр вместимости для фильтрации комнат
+     * @param hasProjector  опциональный параметр проектора для фильтрации комнат
+     * @param hasComputers  опциональный параметр компьютеров для фильтрации комнат
+     * @return список комнат со статусами
+     */
     public List<RoomStatusDTO> getAllRoomsStatus(Integer capacity, Boolean hasProjector, Boolean hasComputers) {
         return null;
     }
