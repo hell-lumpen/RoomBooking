8bd1b46c3775498be901a10378f03a9a57edbe04
Grubenko Maxim <mdgrubenko@maieducation>
2023-10-25 22:25:25 +0300

    АРТЕМ НАХУЙ БЛЯТЬ ДОБАВЛЯЙ @DATA ВСЕМ DTO

--- src/main/java/org/mai/roombooking/dtos/RoomDTO.java
+++ src/main/java/org/mai/roombooking/dtos/RoomDTO.java
@@ -1,8 +1,10 @@
 package org.mai.roombooking.dtos;
 
+import lombok.Data;
 import lombok.NonNull;
 import org.mai.roombooking.entities.Room;
 
+@Data
 public class RoomDTO {
     Long id;
     String name;
