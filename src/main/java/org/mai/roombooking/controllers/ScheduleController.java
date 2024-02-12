9b0ebe32b3b30df112a42937b15bc21f51b8870a
Artem <bam10112002@gmail.com>
2024-02-12 11:17:53 +0300

    add method to Schedule controller

--- src/main/java/org/mai/roombooking/controllers/ScheduleController.java
+++ src/main/java/org/mai/roombooking/controllers/ScheduleController.java
@@ -1,19 +1,20 @@
 package org.mai.roombooking.controllers;
 
+import lombok.AllArgsConstructor;
+import org.mai.roombooking.services.Shedule.ScheduleService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/schedule")
+@AllArgsConstructor
 public class ScheduleController {
-//    private final ParserService parserService;
-//    public ScheduleController(ParserService parserService) {
-//        this.parserService = parserService;
-//    }
-//
-//    @PostMapping("/start")
-//    public void startParsing() {
-//        parserService.startParsing();
-//    }
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping("/start")
+    public void startParsing() {
+        scheduleService.updateSchedule("src/main/resources/groups.xlsx");
+    }
 }
